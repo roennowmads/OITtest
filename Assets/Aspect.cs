9c7ab8ededd73a549c60caa0f129bbc90ffb657e
Mads Rønnow <roennowmads@gmail.com>
2017-11-17 20:01:59 +0100

    Initial commit

---
+++ Assets/Aspect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aspect : MonoBehaviour {
+
+    public Camera mainCam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {                   
+        transform.localScale = new Vector3(mainCam.aspect, 1.0f, 1.0f);
+	}
+}
---
+++ Assets/RenderTexScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderTexScript : MonoBehaviour {
+
+    public RenderTexture renderTex;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     private void OnRenderObject() {
+
+        //m_blendMat.SetTexture("_AccumTex", m_accumTex);
+        //m_blendMat.SetTexture("_RevealageTex", m_revealageTex);
+
+        Graphics.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), renderTex);
+
+        //Graphics.DrawTexture(new Rect(0, 0, Screen.width / 2, Screen.height), m_blendMat);  // be aware that this call seems to fuck the particles up, if the texture is shown on a plane it looks different.
+    }
+}
